d481d75949e5cd6e74954f064656e884ee88688a
kelthuzadx <1948638989@qq.com>
2020-05-17 21:37:20 +0800

    [interpreter] while statement

--- nyx/runtime/Builtin.h
+++ nyx/runtime/Builtin.h
@@ -3,8 +3,14 @@
 
 struct Object;
 
+//===----------------------------------------------------------------------===//
+// Print arbitrary arguments to standard output device
+//===----------------------------------------------------------------------===//
 extern "C" void nyxffi_print(int argc, Object **argv);
 
+//===----------------------------------------------------------------------===//
+// Print arbitrary arguments to standard output device with a line break char
+//===----------------------------------------------------------------------===//
 extern "C" void nyxffi_println(int argc, Object **argv);
 
 
